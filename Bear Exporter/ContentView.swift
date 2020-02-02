d0c86f02acc2c071ce59780876babafaa07fd824
Johnny Sheeley <sheeley@aigee.org>
2020-02-02 09:59:41 -0800

    Initial Commit

---
+++ Bear Exporter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Bear Exporter
+//
+//  Created by Johnny Sheeley on 2/2/20.
+//  Copyright © 2020 Johnny Sheeley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
